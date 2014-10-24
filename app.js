78e2ea7965e721dd4d87b794d059e5172e4b0b0a
Alejandro Vidal Rodriguez <goofyahead@gmail.com>
2014-10-25 00:42:24 +0200

    check timestamps

--- app.js
+++ app.js
@@ -11,7 +11,21 @@ var lodash  = require('lodash').noConflict();
 var _ = require('underscore');
 var async = require('async');
 var uuid = require('node-uuid');
+var fumpers = {};
+var THRESHOLD = 1000;
 
+function check(currentTimeStamp, currentId, response) {
+    console.log('number of fumps in comparison ' + Object.keys(fumpers).length);
+    for (var key in fumpers) {
+        console.log("compairing: " + Math.abs(currentTimeStamp - fumpers[key].timeStamp));
+        if (Math.abs(currentTimeStamp - fumpers[key].timeStamp) < THRESHOLD && currentId != fumpers[key].username) {
+            var elementResponse = {timeStamp : fumpers[key].timeStamp, username : fumpers[key].username };
+            response.push(elementResponse);
+            console.log("added to reponse one match ".green + JSON.stringify(fumpers[key]));
+        }
+    }
+    console.log('finished checking fumpers');
+}
 
 app.set('port', process.env.PORT ||  7000);
 
@@ -22,6 +36,10 @@ io.sockets.on('connection', function (socket) {
     socket.on('message', function (data) { //any object
         data = JSON.parse(data);
         console.log('Message: ', data);
+        fumpers[data.username] = data;
+        var response = [];
+        check(data.timeStamp, data.username, response);
+        console.log(response);
     });
 
     socket.on('disconnect', function () {
@@ -31,7 +49,7 @@ io.sockets.on('connection', function (socket) {
 
 app.post('/api/readings', function(req, res){
 	console.log('post readings');
-	console.log(req);
+	// console.log(req);
 	res.end('ok');
 		
 });
